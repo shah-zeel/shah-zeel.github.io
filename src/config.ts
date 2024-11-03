744f2b337415d428c2fce974ef5821b00ffda499
shah-zeel <zeelshah2708@gmail.com>
2024-11-03 11:39:29 -0700

    Updates

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
+export const SITE_IMAGE = '/ZS.png';
 export const SITE_TITLE = 'Zeel Shah | Personal Portfolio Website';
 export const SITE_DESCRIPTION = 'Welcome to my personal website!';
 export const GENERATE_SLUG_FROM_TITLE = true
